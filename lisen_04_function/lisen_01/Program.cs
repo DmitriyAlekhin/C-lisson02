8208253d315375a5219a2b68c00f7a05127e49c3
big_kitty <dimedrol3004@gmail.com>
2024-01-27 20:14:38 +0900

    Исправил lisen 1

--- lisen_04_function/lisen_01/Program.cs
+++ lisen_04_function/lisen_01/Program.cs
@@ -1,12 +1,30 @@
-﻿// Задача 1: Напишите программу, которая бесконечно
-// запрашивает целые числа с консоли. Программа
-// завершается при вводе символа ‘q’ или при вводе
-// числа, сумма цифр которого четная.
+﻿// Вопрос почемму если перенести функцию puch() в [1] возникает ошибка.
+// eeror: Невозможно использовать локальную переменную "array" перед ее объявлением
+
+void  Rundom_Number(int[] arr) {
+	int i = 0;
+	while(i <= arr.Length - 1){
+		arr[i] = new Random().Next(1,100);
+		i++;
+	}
+}
+
+// ссылка -> [1] место для переноса функции
+
+
+int[] array = new int[1];
+Console.WriteLine($"Начальный масив: \t\t\t [{string.Join(",", array)}]");
+
+Rundom_Number(array);
+Console.WriteLine($"Начальный масив стал ранномным: \t [{string.Join(",", array)}]");
+
+puch(ref array, 10);
+Console.WriteLine($"Новый масив: \t\t\t\t [{string.Join(",", array)}]");
+
+
 
 
 int caunt = 1;
-int[] array = new int[caunt];
-
 while(true){
 	Console.Write($"Введите [{caunt - 1}] элемент масива: ");
 	string read = Console.ReadLine()!;
@@ -19,18 +37,64 @@ while(true){
 	Array.Resize(ref array,caunt);
 }
 
-
-Console.Write(string.Join(",", array));
-
+Console.WriteLine($"Введённый массив: \t\t\t [{string.Join(",", array)}]");
 
 
 
+// фкнкция push() Перенесите её в место для вставки 
+void puch(ref int[] arr, int n)
+{
+	int[] new_array = new int[n];
+	for (int i = 0; i < array.Length; i++){
+		new_array[i] = array[i];
+	}
+	array = new_array;
+}
 
 
 
 
 
 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// int caunt = 1;
+// int[] array = new int[caunt];
+
+// while(true){
+// 	Console.Write($"Введите [{caunt - 1}] элемент масива: ");
+// 	string read = Console.ReadLine()!;
+// 	if(read == "q" || int.Parse(read) % 2 == 0 && int.Parse(read)/10 != 0)	{
+// 		puch(ref array,caunt - 1);
+// 		break;
+// 	}
+// 	array[caunt - 1] = int.Parse(read);
+// 	caunt++;
+// 	puch(ref array,caunt);
+// }
+
+
+// // Console.Write(string.Join(",", array));
 
 
 // void puch(ref int[] arr, int n){  // Array.Resize(ref array,10); --> ???
@@ -42,5 +106,6 @@ Console.Write(string.Join(",", array));
 // }
 // puch(ref array, 10);
 
-// Array.Resize(ref array,10);
+// // Array.Resize(ref array,10);
+
 
