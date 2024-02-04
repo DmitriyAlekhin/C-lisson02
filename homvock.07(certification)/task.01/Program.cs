651f1b306ca4b610664b085ab8434ceb561ff48b
big_kitty <dimedrol3004@gmail.com>
2024-02-04 16:29:01 +0900

    add Задание промежуточной аттестации

---
+++ homvock.07(certification)/task.01/Program.cs
@@ -0,0 +1,26 @@
+﻿// Задача 1: Задайте значения M и N. 
+// Напишите программу, которая выведет все натуральные числа 
+// в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+
+int M,N;
+
+M = 1;
+M++;
+N = 10;
+
+Console.ForegroundColor = ConsoleColor.Yellow;
+Console.WriteLine($" Начало диопозона: {M} \n Конец Диапозона: {N}");
+Console.ResetColor();
+	
+my_while(M,N);
+
+void my_while(int M, int N){
+	if(M == N){
+		return;
+	}
+	Console.Write($" {M}");
+	
+	my_while(M + 1,N);
+}
---
+++ homvock.07(certification)/task.03/Program.cs
@@ -0,0 +1,29 @@
+﻿// Задача 3: Задайте произвольный массив.
+//  Выведете его элементы, начиная с конца.
+//  Использовать рекурсию, не использовать циклы.
+
+
+int [] array = new int [10]{0,3,9,1,1,2,3,7,6,10};
+
+Console.WriteLine($"Начальный мосив:\t[{string.Join(" ", array)}]");
+Console.Write("Конечный мосив: \t[");
+print_array_revers(array);
+Console.Write("]");
+
+
+
+void print_array_revers(int[] array, int i = 0){
+	if(i > array.Length - 1){
+		return;
+	}
+	print_array_revers(array, i + 1);
+
+	// Код ниже просто улучшает вывод. Достаточно будет :
+	// Console.Write($"{array[i]} ");
+	if(i == 0)
+		Console.Write($"{array[i]}");
+	else
+		Console.Write($"{array[i]} ");
+	
+}
+
